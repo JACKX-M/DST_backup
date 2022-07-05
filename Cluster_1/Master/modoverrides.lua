b620fbd97690e5431cd8cd1c8f299af4d9c397b8
ubuntu <ubuntu@localhost.localdomain>
2022-07-05 20:42:21 +0800

    log message

---
+++ Cluster_1/Master/modoverrides.lua
@@ -0,0 +1,69 @@
+return {
+  ["workshop-1185229307"]={
+    configuration_options={
+      [""]="",
+      DAMAGE_NUMBERS=true,
+      DAMAGE_RESISTANCE=true,
+      FRAME_PHASES=true,
+      HORIZONTAL_OFFSET=0,
+      NOEPIC=false,
+      NONOEPIC=true,
+      WETNESS_METER=false 
+    },
+    enabled=true 
+  },
+  ["workshop-2075943614"]={ configuration_options={  }, enabled=true },
+  ["workshop-2323750553"]={
+    configuration_options={ DropWholeStack=true, MAXSTACKSIZE=99, auto_stack=true },
+    enabled=true 
+  },
+  ["workshop-2527328931"]={
+    configuration_options={
+      Animal=true,
+      BuildRepair=true,
+      ChopTime=0.01,
+      CookTime=0,
+      DryTime=0,
+      FishTime=999,
+      HSHU=true,
+      MineTime=0.01,
+      Others=true,
+      Pick=true,
+      Plant=true 
+    },
+    enabled=true 
+  },
+  ["workshop-347079953"]={
+    configuration_options={ DFV_Language="EN", DFV_MinimalMode="default" },
+    enabled=true 
+  },
+  ["workshop-356930882"]={
+    configuration_options={ uses=10000000, uses2=10000000, uses3=10000000 },
+    enabled=true 
+  },
+  ["workshop-375859599"]={
+    configuration_options={
+      divider=5,
+      random_health_value=0,
+      random_range=0,
+      send_unknwon_prefabs=false,
+      show_type=0,
+      unknwon_prefabs=1,
+      use_blacklist=true 
+    },
+    enabled=true 
+  },
+  ["workshop-378160973"]={
+    configuration_options={
+      ENABLEPINGS=true,
+      FIREOPTIONS=2,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=2 
+    },
+    enabled=true 
+  },
+  ["workshop-856487758"]={ configuration_options={  }, enabled=true } 
+}
---
+++ Cluster_3/Master/modoverrides.lua
@@ -0,0 +1,45 @@
+return {
+  ["workshop-1185229307"]={
+    configuration_options={
+      [""]="",
+      DAMAGE_NUMBERS=true,
+      DAMAGE_RESISTANCE=true,
+      FRAME_PHASES=true,
+      HORIZONTAL_OFFSET=0,
+      NOEPIC=false,
+      NONOEPIC=false,
+      WETNESS_METER=false 
+    },
+    enabled=true 
+  },
+  ["workshop-2075943614"]={ configuration_options={  }, enabled=true },
+  ["workshop-347079953"]={
+    configuration_options={ DFV_Language="EN", DFV_MinimalMode="default" },
+    enabled=true 
+  },
+  ["workshop-375859599"]={
+    configuration_options={
+      divider=5,
+      random_health_value=0,
+      random_range=0,
+      send_unknwon_prefabs=false,
+      show_type=0,
+      unknwon_prefabs=1,
+      use_blacklist=true 
+    },
+    enabled=true 
+  },
+  ["workshop-378160973"]={
+    configuration_options={
+      ENABLEPINGS=true,
+      FIREOPTIONS=2,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=2 
+    },
+    enabled=true 
+  },
+  ["workshop-856487758"]={ configuration_options={  }, enabled=true } 
+}
